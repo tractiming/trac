f86228ed4a565510e2b5d0d897bde2308b51d98c
elliot <ejhevel@gmail.com>
2014-09-16 20:22:59 -0500

    saving some initial attempts to get django running on google app engine

--- dev/tracd/common/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.template import RequestContext
-
-def index(request):
-    context = RequestContext(request)
-    context_dict = {}
-    return render(request, 'index.html', {})
--- dev/tracd/results/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-from common.models import Workout
-
-
-#class WorkoutLog(Workout):
-#    """A workout log is a workout with additional information."""
-#    owner = models.ForeignKey(User)
